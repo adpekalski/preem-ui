10cd2e6a3c4654364f4fd43a28112fee047cb08c
A. Pękalski <adpekalski@gmail.com>
2024-01-22 12:22:50 +0100

    First base version
    *Changed rollup to vite
    *Added styled components after changing css choice from pandaCSS
    *Added storybook
    *Changed typescript config
    *Added eslint
    *First base configuration for storybook
    
    *Added first component in it's first iteration - button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { MouseEventHandler, ReactNode } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-export interface ButtonProps {
-    label: string;
+export type ButtonProps = {
+    // children: ReactNode;
+    text?: string;
+    primary?: boolean;
+    disabled?: boolean;
+    size?: "small" | "medium" | "large";
+    onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-const Button = (props: ButtonProps) => {
+const StyledButton = styled(motion.button)<ButtonProps>`
+  border: 0;
+  line-height: 1;
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: 700;
+  font-weight: bold;
+  border-radius: 10px;
+  display: inline-block;
+  color: ${(props) => (props.primary ? "#fff" : "#000")};
+  background-color: ${(props) => (props.primary ? "#FF5655" : "#f4c4c4")};
+  padding: ${(props) =>
+        props.size === "small"
+            ? "7px 25px 8px"
+            : props.size === "medium"
+                ? "9px 30px 11px"
+                : "14px 30px 16px"};
+`;
+
+const Button: React.FC<ButtonProps> = ({
+    size,
+    primary,
+    disabled,
+    text,
+    onClick,
+    ...props
+}) => {
     return (
-        <button>
-            {props.label}
-        </button>
+        <StyledButton
+            type="button"
+            onClick={onClick}
+            primary={primary}
+            disabled={disabled}
+            size={size}
+            {...props}
+            animate={{
+                scale: 2
+            }}
+            transition={{
+                duration: 10
+            }}
+        >
+            {text}
+        </StyledButton>
     );
 };
 
